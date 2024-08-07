454071ff59fcb1998ffc829abdc7c200577b71db
ChinmayEnwheel <organellefarms123@gmail.com>
2024-08-07 10:44:22 +0530

    fixing

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 const express = require("express");
 const fs = require("fs");
-const users = require("./MOCK_DATA.json");
+// const users = require("./MOCK_DATA.json");
 const { error } = require("console");
+const mongoose = require("mongoose");
+const { type } = require("os");
 const app = express();
 const PORT= 8000;
+
+//Connecting mongodb
+const dbConnection = ()=>{
+    mongoose.connect("mongodb://127.0.0.1:27017/my-app")
+    .then(()=>
+        console.log("mongo db connection is succesful")
+    ).catch((err)=>console.log("Mongodb Error",err))
+}
+dbConnection();
+
+
+//Defining Schema....
+const UserSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type:String,
+        required: false,
+    },
+    Email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    jobTitle:{
+        type:String,
+
+    },
+    gender:{
+        type:String,
+
+    }
+},
+{timestamps:true}) 
+//Defining UserModel.... 
+const User = mongoose.model("user",UserSchema);
+
+
+
 
 
 //Middleware Or plugin
@@ -28,11 +71,12 @@ app.use((req, res, next)=>{
     next();
 })
 //Routes
-app.get("/users", (req,res)=>{
+app.get("/users",async (req,res)=>{
+    const allDbUsers = await User.find({});
     const html = `
     <ul>
-    ${users.map((user)=>
-        `<li>${user.first_name}</li>`
+    ${allDbUsers.map((user)=>
+        `<li>${user.firstName} - ${user.Email}</li>`
     ).join("")}
     </ul>
     `;
@@ -42,11 +86,12 @@ app.get("/users", (req,res)=>{
 
 
 //Rest Api
-app.get("/api/users" , (req, res)=>{
+app.get("/api/users" ,async (req, res)=>{
+    const allDbUsers = await User.find({});
     res.setHeader("X-myname","chinmay");
     // Always add a X to the custoom headers...
     console.log(req.headers);
-  return res.json(users);
+  return res.json(allDbUsers);
 })
 
 // app.get("/api/users/:id" , (req,res)=>{
@@ -57,13 +102,18 @@ app.get("/api/users" , (req, res)=>{
 
 // })
 
-
+//Find by id
 app.route("/api/users/:id")
-.get((req,res)=>{
-    const id= Number(req.params.id);
-    // console.log(id);
-    const myuser = users.find((user)=>user.id === id);
-    return res.json(myuser);
+.get(async (req,res)=>{
+    const user = await User.findById(req.params.id);
+    if(!user){
+        return res.status(404).json({error:"Users not found or User doesnot exist"});
+    }
+    return res.json(user);
+    // const id= Number(req.params.id);
+    // // console.log(id);
+    // const myuser = users.find((user)=>user.id === id);
+    // return res.json(myuser);
 })
 .patch((req,res)=>{
 
@@ -91,22 +141,42 @@ app.route("/api/users/:id")
 
 
 //Now express don't know how to handle the datacome from the front end thats why we need to use the MIddleware(Plugins)
-app.post("/api/users",(req,res)=>{
+app.post("/api/users",async (req,res)=>{
 
     const body = req.body;
+    if (!body||!body.first_name||!body.last_name||!body.email||!body.job_title||!body.gender)
+        {
+        res.status(400).json({msg:"All fields are required"});
+    }
+const result = await User.create({
+    firstName:body.first_name,
+    lastName:body.last_name,
+    Email:body.email,
+    jobTitle:body.job_title,
+    gender:body.gender,
+})
+console.log("result",result);
+return res.status(201).json({ msg:"success"})
+   
+
+
+
+
+
+
     //Push tthe data to the users.. Come from the frontend as postman get request using a urlencoded middleware
-    users.push(
-       {...body , id:users.length +1} 
-    );
-    console.log("Body" , body);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
-        if(err){
-            console.log(err);
-            return res.status(500).json({status:"error"});
-        }
-        return res.json({status:"Sucess", id:users.length});
+    // users.push(
+    //    {...body , id:users.length +1} 
+    // );
+    // console.log("Body" , body);
+    // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
+    //     if(err){
+    //         console.log(err);
+    //         return res.status(500).json({status:"error"});
+    //     }
+    //     return res.json({status:"Sucess", id:users.length});
         
-    })
+    // })
     
     // return res.json({status:"Pending"});
 })  
